70d9ed5e41836fa667396ca9ef6a5a77d43c1e1c
Lowrance, Peter <PLowrance@veritegroup.com>
2023-02-08 09:59:43 -0500

    Add vulnerable bank favicon

--- vulnerable-bank/mysite/settings.py
+++ vulnerable-bank/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'website/react-website/static')]
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'website/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
